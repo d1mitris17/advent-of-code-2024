81f8b43edf06d693b360e2a23f8b8da1efbee541
Dimitris Doukas <dd2040@hw.ac.uk>
2024-12-11 05:03:27 +0000

    made a start on day 2 problem

---
+++ day-2/day2.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class day2 {
+
+	public static void main(String[] args) {
+		// Read the file
+		ArrayList<Integer[]> data = day2_file_read.read_file("input.txt");
+	}
+
+	// TODO: Implement the part1 function
+	// so that if 2 adajacent values have a difference less than or eequal to 2 then
+	// it is safe
+	// and all the numbers must either be decreasing or increasing
+	public static int part1(ArrayList<Integer[]> data) {
+		int safe_levels = 0;
+		for (Integer[] level : data) {
+
+		}
+	}
+}
---
+++ day-2/day2_file_read.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+
+/**
+ * day2_file_read
+ */
+public class day2_file_read {
+
+	public static ArrayList<Integer[]> read_file(String file_name) {
+		ArrayList<Integer[]> data = new ArrayList<Integer[]>();
+		try {
+			FileReader file = new FileReader(file_name);
+			BufferedReader reader = new BufferedReader(file);
+			String line = reader.readLine();
+			while (line != null) {
+				String[] parts = line.trim().split("\\s+");
+				Integer[] line_data = Arrays.stream(parts).map(Integer::parseInt)
+						.toArray(Integer[]::new);
+				data.add(line_data);
+				for (Integer i : line_data) {
+					System.out.print(i + " ");
+				}
+				System.out.println();
+				line = reader.readLine();
+			}
+			reader.close();
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+		return data;
+	}
+}
