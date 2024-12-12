c92d4a7cd3722c9b7c24dc23c4600c87ead92dad
Dimitris Doukas <dd2040@hw.ac.uk>
2024-12-12 23:13:34 +0000

    Feat: Reads input from file

---
+++ day-5/day5.java
@@ -0,0 +1,52 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+
+class day5 {
+	public static ArrayList<int[]> rules = new ArrayList<int[]>();
+	public static ArrayList<int[]> pageNumbers = new ArrayList<int[]>();
+
+	public static void main(String[] args) {
+
+	}
+
+	public static void readFile() {
+		try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
+			String line = reader.readLine();
+
+			while (line != null) {
+				try {
+					String[] parts = line.split("\\|");
+					int[] rule = new int[parts.length];
+					for (int i = 0; i < parts.length; i++) {
+						rule[i] = Integer.parseInt(parts[i].trim());
+					}
+					rules.add(rule);
+				} catch (NumberFormatException e) {
+					e.printStackTrace();
+				}
+				line = reader.readLine();
+			}
+
+			line = reader.readLine();
+
+			while (line != null) {
+				try {
+					String[] parts = line.split(",");
+					int[] update = new int[parts.length];
+					for (int i = 0; i < parts.length; i++) {
+						update[i] = Integer.parseInt(parts[i].trim());
+					}
+					pageNumbers.add(update);
+				} catch (NumberFormatException e) {
+					e.printStackTrace();
+				}
+				line = reader.readLine();
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
